cb3a77d05a1345f9beffe2a08f245528e51c4750
fanilinho22 <guaranidapalhoca@gmail.com>
2023-10-02 11:48:43 -0300

    x

--- src/App.js
+++ src/App.js
@@ -3,23 +3,134 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="index">
+      <div className="div">
+        <div className="overlap-group">
+          <img
+            className="vector"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/vector.svg"
+          />
+          <div className="rectangle" />
+          <img
+            className="img"
+            alt="Vector"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/vector-1.svg"
+          />
+          <img
+            className="rectangle-2"
+            alt="Rectangle"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/rectangle-4.gif"
+          />
+          <img
+            className="rectangle-3"
+            alt="Rectangle"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/rectangle-5.gif"
+          />
+          <img
+            className="rectangle-4"
+            alt="Rectangle"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/rectangle-7.gif"
+          />
+          <img
+            className="rectangle-5"
+            alt="Rectangle"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/rectangle-6.gif"
+          />
+          <p className="text-wrapper">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tellus nulla, mollis consectetur lobortis et,
+            consequat ut eros. Vestibulum ac mi nec velit bibendum consequat. Nam nec eleifend mauris, eu sagittis nisi.
+          </p>
+          <img
+            className="rectangle-6"
+            alt="Rectangle"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/rectangle-8.png"
+          />
+          <div className="text-wrapper-2">palavra</div>
+          <div className="text-wrapper-3">palavra</div>
+          <div className="text-wrapper-4">palavra</div>
+          <div className="text-wrapper-5">palavra</div>
+          <p className="exemplo-de-como">
+            Exemplo de como
+            <br />
+            ficaria um pequeno
+            <br />
+            texto. Mais algumas
+            <br />
+            palavras.
+          </p>
+          <p className="p">
+            Exemplo de como
+            <br />
+            ficaria um pequeno
+            <br />
+            texto. Mais algumas
+            <br />
+            palavras.
+          </p>
+          <p className="exemplo-de-como-2">
+            Exemplo de como
+            <br />
+            ficaria um pequeno
+            <br />
+            texto. Mais algumas
+            <br />
+            palavras.
+          </p>
+          <p className="exemplo-de-como-3">
+            Exemplo de como
+            <br />
+            ficaria um pequeno
+            <br />
+            texto. Mais algumas
+            <br />
+            palavras.
+          </p>
+          <img
+            className="polygon"
+            alt="Polygon"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/polygon-1.svg"
+          />
+          <img
+            className="line"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/line-6.svg"
+          />
+          <img
+            className="line-2"
+            alt="Line"
+            src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/line-7.svg"
+          />
+        </div>
+        <div className="overlap">
+          <div className="div-wrapper">
+            <div className="text-wrapper-6">Log In</div>
+          </div>
+          <div className="overlap-2">
+            <div className="text-wrapper-7">Bonnape</div>
+            <div className="text-wrapper-8">Bonnape</div>
+          </div>
+          <div className="overlap-3">
+            <div className="rectangle-7" />
+            <div className="text-wrapper-9">Quem somos</div>
+            <div className="text-wrapper-10">Quem somos</div>
+          </div>
+        </div>
+        <img
+          className="cafe"
+          alt="Cafe"
+          src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/cafe.gif"
+        />
+        <div className="ellipse" />
+        <img
+          className="chevron-up"
+          alt="Chevron up"
+          src="https://cdn.animaapp.com/projects/651ad25e56fabe7315983286/releases/651ad2f3201019696ae0e686/img/chevron-up.svg"
+        />
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
